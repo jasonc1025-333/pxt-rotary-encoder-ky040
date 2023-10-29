deb8ec909e1dbc2d33df8cbc5ca20864289d5844
jasonc1025-333 <67285598+jasonc1025-333@users.noreply.github.com>
2023-10-29 04:58:16 -0700

    23-1029-0500 'tests.ts'

--- tests.ts
+++ tests.ts
@@ -1,20 +1,31 @@
 // tests go here; this will not be compiled when this package is used as a library
 
-let item = 5;
-RotaryEncoder.init(DigitalPin.P8, DigitalPin.P9, DigitalPin.P11);
-basic.forever(() => {
-    basic.showNumber(item);
+let encoder_A_C1_DigitalValue_StateRiseFallChange_Count_2_Int = 0;
+let encoder_B_C2_DigitalValue_StateRiseFallChange_Count_2_Int = 0;
+
+RotaryEncoder.init_Encoder_AnalogPins_Fn(AnalogPin.P0, AnalogPin.P1);
+
+RotaryEncoder.on_EncoderNewState_Event_Fn(1000, () => {
+    serial.writeLine("***** ***** RotaryEncoder.on_EncoderNewState_Event_Fn: " + convertToText(encoder_B_C2_DigitalValue_StateRiseFallChange_Count_2_Int));
 })
-RotaryEncoder.onPressEvent(() => {
-    //serial.writeString("onPress\n");
-    item = 5;
-    basic.showIcon(IconNames.Heart);
-})
-RotaryEncoder.onRotateEvent(RotationDirection.Right, () => {
-    //serial.writeString("rotate right\n");
-    item++;
-})
-RotaryEncoder.onRotateEvent(RotationDirection.Left, () => {
-    //serial.writeString("rotate left\n");
-    item--;
-})
+
+
+/// jwc o let item = 5;
+/// jwc o 
+/// jwc o RotaryEncoder.init(DigitalPin.P8, DigitalPin.P9, DigitalPin.P11);
+/// jwc o basic.forever(() => {
+/// jwc o     basic.showNumber(item);
+/// jwc o })
+/// jwc o RotaryEncoder.onPressEvent(() => {
+/// jwc o     //serial.writeString("onPress\n");
+/// jwc o     item = 5;
+/// jwc o     basic.showIcon(IconNames.Heart);
+/// jwc o })
+/// jwc o RotaryEncoder.onRotateEvent(RotationDirection.Right, () => {
+/// jwc o     //serial.writeString("rotate right\n");
+/// jwc o     item++;
+/// jwc o })
+/// jwc o RotaryEncoder.onRotateEvent(RotationDirection.Left, () => {
+/// jwc o     //serial.writeString("rotate left\n");
+/// jwc o     item--;
+/// jwc o })
